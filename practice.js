9e4a81f649c3645d02e7eb3d13f149fcbfb0fdb1
Sagnik004 <sagnikchakraborty910@yahoo.com>
2021-01-10 17:24:09 +0530

    10-Jan-2021 Phase-1.1

--- practice.js
+++ practice.js
@@ -4,9 +4,9 @@
 // Array Rotation ( link: https://www.geeksforgeeks.org/array-rotation/ )      //
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
 
-/*
-const arr = [1, 2, 3, 4, 5, 6, 7];
-const d = 2; */
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// const rotateBy = 2;
 
 // function rotateMyArray(arr, d, n) {
 /* ================== My First Approach ========================= TC: O(n), SC: O(d) */
@@ -65,12 +65,66 @@ for (let i = 0; i < g_c_d; i++) {
     j = k;
   }
   arr[j] = temp;
+} */
+
+/* ================= 4th Approach (Reversal Algorithm) ============ TC: O(n) SC: O(1) */
+  // function reverseArray(start, end) {
+  //   while (start < end) {
+  //     let temp = arr[start];
+  //     arr[start] = arr[end];
+  //     arr[end] = temp;
+  //     start++;
+  //     end--;
+  //   }
+  // }
+
+  // First checks
+  // if (d === 0) return;
+  // d = d % n; // in case rotating length is greater than array length
+
+  // reverseArray(0, d - 1);
+  // reverseArray(d, n - 1);
+  // reverseArray(0, n - 1);
+
+/* ================= 4.1) Rotate From Right (Reversal Algorithm) ============ TC: O(n) SC: O(1) */
+/* ============ https://www.geeksforgeeks.org/reversal-algorithm-right-rotation-array/?ref=rp ============ */
+  // reverseArray(0, n - d - 1);
+  // reverseArray(n - d, n - 1);
+  // reverseArray(0, n - 1);
+  // Another approach can be - reverseArray(0, n-1) -> reverseArray(0, d-1), reverseArray(d, n-1)
+
+
+/* ================= 5th Approach (Block swap Algorithm) ============ TC: O(n) SC: O(1) */
+/* ============ https://www.geeksforgeeks.org/block-swap-algorithm-for-array-rotation/ ============ */
+/* function swap(fi, si, di) {
+  let temp;
+  for (let i = 0; i < di; i++) {
+    temp = arr[fi + i];
+    arr[fi + i] = arr[si + i];
+    arr[si + i] = temp;
+  }
 }
+
+function leftRotateRec(i, d, n) {
+  if (d === 0 || d === n) return; // Basic validation
+
+  if ((n - d) === d) { // Exactly in middle
+    swap(i, n - d + i, d);
+  } else if (d < (n - d)) { // A is shorter
+    swap(i, n - d + i, d);
+    leftRotateRec(i, d, n - d);
+  } else { // B is shorter
+    swap(i, d, n - d);
+    leftRotateRec(n - d + i, (2 * d) - n, d);
+  }
+}
+
+leftRotateRec(0, d, n);
 
 console.log(arr);
 }
 
-rotateMyArray(arr, d, arr.length); */
+rotateMyArray(arr, rotateBy, arr.length); */
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ //
 //                    Reversal Algorithm for Array Rotation                    //       
