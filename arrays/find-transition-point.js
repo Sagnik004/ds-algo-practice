7ceb2d50a4fe533a57261c9965956cde44c35126
Sagnik004 <sagnikchakraborty910@yahoo.com>
2021-06-28 17:48:28 +0530

    arr findTransitionPoint better approach

--- arrays/find-transition-point.js
+++ arrays/find-transition-point.js
@@ -19,28 +19,27 @@ console.log(transitionPoint([0, 0, 0, 0]));
 
 
 // Method 2: Using binary search with recursion. TC = O(log n), SC = O(1)
-function transitionPointWithBinarySearch(arr, start, end) {
-    if (start > end) {
-      return -1;
+function transitionPointWithBinarySearch(arr) {
+    let start = 0, end = (arr.length - 1);
+
+    while (start <= end) {
+      let mid = Math.floor((start + end) / 2);
+
+      if (arr[mid] === 0) {
+        start = mid + 1;
+      } else if(arr[mid] === 1) {
+        if (mid === 0 || (mid > 0 && arr[mid - 1] === 0)) {
+          return mid;
+        }
+        end = mid - 1;
+      }
     }
 
-    const mid = Math.floor((start + end) / 2);
-
-    if (arr[mid] === 0) {
-      if (arr[mid + 1] === 1) {
-        return (mid + 1);
-      }
-      return transitionPointWithBinarySearch(arr, mid + 1, end);
-    } else if (arr[mid] === 1) {
-      if (arr[mid - 1] === 0) {
-        return mid;
-      }
-      return transitionPointWithBinarySearch(arr, start, mid - 1);
-    }
+    return -1;
 }
 
-console.log(transitionPointWithBinarySearch([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1], 0, 10));
-console.log(transitionPointWithBinarySearch([0, 0, 0, 0], 0, 3));
-console.log(transitionPointWithBinarySearch([1, 1, 1, 1], 0, 3));
-console.log(transitionPointWithBinarySearch([0, 0, 0, 1, 1], 0, 4));
-console.log(transitionPointWithBinarySearch([0, 0, 0, 0, 1, 1, 1, 1], 0, 7));
+console.log(transitionPointWithBinarySearch([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1]));
+console.log(transitionPointWithBinarySearch([0, 0, 0, 0]));
+console.log(transitionPointWithBinarySearch([1, 1, 1, 1])); // Here 0th position will be returned, if we don't want it then remove the check of mid = 0
+console.log(transitionPointWithBinarySearch([0, 0, 0, 1, 1]));
+console.log(transitionPointWithBinarySearch([0, 0, 0, 0, 1, 1, 1, 1]));
